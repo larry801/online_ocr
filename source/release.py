60d7cab574b6f34fef1189c5ad8e53d5bc0f72cd
larry801 <larry.wang.801@gmail.com>
2019-04-02 19:37:37 +0800

    Change appveyor config

---
+++ source/release.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+# Copyright (C) 2019 Larry Wang <larry.wang.801@gmail.com>
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+import buildVars
+import os
+import subprocess
+
+version = buildVars.addon_info["addon_version"]
+subprocess.call([
+	"git",
+	"tag",
+	'-m',
+	'"{0}"'.format(version),
+	version
+])
+subprocess.call([
+	"git", "push", "--tags"
+])
